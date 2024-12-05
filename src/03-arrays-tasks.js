4e220789e94b029ba6c7ab8e4c2d96dca89ced73
DmitryTech-jpg <lbvfcthuttd1234@gmail.com>
2024-12-05 20:46:42 +0400

    arr reduce

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -236,8 +236,13 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  return arr.reduce((accumulator, currentValue) => {
+    const newSum = (accumulator.length > 0
+      ? accumulator[accumulator.length - 1] : 0) + currentValue;
+    accumulator.push(newSum);
+    return accumulator;
+  }, []);
 }
 
 
