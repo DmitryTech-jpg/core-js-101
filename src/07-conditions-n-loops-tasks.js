d4935e6dcec842a90b0c5c515bfef65132d62de7
Dmitry Sergeev <lbvfcthuttd1234@gmail.com>
2024-12-16 16:36:04 +0400

    return sum > 9

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -310,8 +310,12 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sum = num.toString().split('').reduce((acc, digit) => acc + Number(digit), 0);
+  while (sum > 9) {
+    sum = sum.toString().split('').reduce((acc, digit) => acc + Number(digit), 0);
+  }
+  return sum;
 }
 
 
