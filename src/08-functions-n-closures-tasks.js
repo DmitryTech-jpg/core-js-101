d4f8c6a503a65484795df308577ada3a20e80043
Dmitry Sergeev <lbvfcthuttd1234@gmail.com>
2024-12-17 14:49:42 +0400

    return id generator

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -179,8 +179,19 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let collDown = 0;
+  return function () {
+    let result = 0;
+    if (collDown === 0) {
+      result = startFrom + collDown;
+      collDown += 1;
+    } else {
+      result = startFrom + collDown;
+      collDown += 1;
+    }
+    return result;
+  };
 }
 
 
